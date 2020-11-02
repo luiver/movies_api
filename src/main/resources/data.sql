c449507901c90d5633280020e0e13955d7f0b4c8
MateuszGolda <mateusz.golda1993@gmail.com>
2020-11-02 13:55:39 +0100

    populate db from sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into countries(name)
+values ('Poland');
+
+insert into people(bio, date_of_birth, name, surname, country_id)
+values ('bio', '1962-06-13', 'Cezary', 'Pazura', 1);
+
+insert into people(bio, date_of_birth, name, surname, country_id)
+values ('bio', '1952-06-27', 'Bogusław', 'Linda', 1);
+
+insert into people(bio, date_of_birth, name, surname, country_id)
+values ('bio', '1950-10-18', 'Marek', 'Kondrat', 1);
