371f35746ababac122d47998cb1b7b097afce877
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-11-02 15:07:40 +0100

    add @Repository annotation

--- src/main/java/com/codecool/moviesapi/dao/PersonRepository.java
+++ src/main/java/com/codecool/moviesapi/dao/PersonRepository.java
@@ -4,7 +4,7 @@ import com.codecool.moviesapi.entity.Person;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository()
 public interface PersonRepository extends CrudRepository<Person, Long> {
 
     Person findByPersonId(Long id);
