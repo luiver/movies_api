37056ecd2bc709530896d009d55500febb3d539c
MateuszGolda <mateusz.golda1993@gmail.com>
2020-11-02 19:02:06 +0100

    fix mapping error in GenreController

--- src/main/java/com/codecool/moviesapi/controller/GenreController.java
+++ src/main/java/com/codecool/moviesapi/controller/GenreController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/people")
+@RequestMapping("/genre")
 public class GenreController extends GenericController<Genre> {
 
     @Autowired
