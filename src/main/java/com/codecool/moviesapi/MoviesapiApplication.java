a0ff072423537e0777c27a6e17c1ff343f2265e5
MateuszGolda <mateusz.golda1993@gmail.com>
2020-11-02 11:22:51 +0100

    change tabs to spaces

--- src/main/java/com/codecool/moviesapi/MoviesapiApplication.java
+++ src/main/java/com/codecool/moviesapi/MoviesapiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MoviesapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoviesapiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesapiApplication.class, args);
+    }
 }
