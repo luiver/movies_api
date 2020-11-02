af4be8773c961f922cad6f7e734712d59077df98
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-11-02 21:39:05 +0100

    add necessary getters

--- src/main/java/com/codecool/moviesapi/entity/MoviePersonRole.java
+++ src/main/java/com/codecool/moviesapi/entity/MoviePersonRole.java
@@ -1,22 +1,32 @@
 package com.codecool.moviesapi.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity(name = "movies_people_roles")
 public class MoviePersonRole implements Serializable {
+
     @Id
     @ManyToOne
+    @JoinColumn(name = "movie_id")
     private Movie movie;
 
     @Id
     @ManyToOne
+    @JoinColumn(name = "person_id")
     private Person person;
 
     @Id
     @ManyToOne
+    @JoinColumn(name = "role_id")
     private Role role;
+
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public Role getRole() {
+        return role;
+    }
 }
