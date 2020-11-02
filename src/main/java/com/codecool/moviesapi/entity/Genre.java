909b1de5a0bd2352660724f7380beb962e318776
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-11-02 13:20:48 +0100

    create getter setters and constructors for Genre class

--- src/main/java/com/codecool/moviesapi/entity/Genre.java
+++ src/main/java/com/codecool/moviesapi/entity/Genre.java
@@ -15,4 +15,36 @@ public class Genre {
 
     @ManyToMany(mappedBy = "genres")
     private Set<Movie> movies;
+
+    public Genre(long genre_id, String name) {
+        this.genre_id = genre_id;
+        this.name = name;
+    }
+
+    public Genre() {
+    }
+
+    public long getGenre_id() {
+        return genre_id;
+    }
+
+    public void setGenre_id(long genre_id) {
+        this.genre_id = genre_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Genre{" +
+                "genre_id=" + genre_id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
