75417a375f8cab01488fc29d2b0f8336af2b8833
sforaaleksander <sforaaleksander@gmail.com>
2020-11-02 12:09:32 +0100

    implement Person.java model

---
+++ src/main/java/com/codecool/moviesapi/entity/Person.java
@@ -0,0 +1,29 @@
+package com.codecool.moviesapi.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity(name="people")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "person_id")
+    private long personId;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 50)
+    private String surname;
+
+    @OneToMany
+    @Column(name = "country_id")
+    private long countryId;
+
+    @Column
+    private String bio;
+
+    @Column(name = "date_of_birth")
+    private Date dateOfBirth;
+}
