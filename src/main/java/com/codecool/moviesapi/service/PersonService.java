47b79ac94a22a4dffa008c5bc2efab5229b1054f
MateuszGolda <mateusz.golda1993@gmail.com>
2020-11-02 17:38:38 +0100

    generify services

--- src/main/java/com/codecool/moviesapi/service/PersonService.java
+++ src/main/java/com/codecool/moviesapi/service/PersonService.java
@@ -1,40 +1,15 @@
 package com.codecool.moviesapi.service;
 
-import com.codecool.moviesapi.dao.PersonRepository;
 import com.codecool.moviesapi.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
-public class PersonService implements GenericService<Person> {
-    private CrudRepository<Person, Long> personRepository;
-
-    public Iterable<Person> getAll() {
-        return personRepository.findAll();
-    }
+public class PersonService extends GenericService<Person> {
 
     @Autowired
-    public void setRepository(CrudRepository<Person, Long> personRepository) {
-        this.personRepository = personRepository;
-    }
-
-    public Optional<Person> getById(Long id) {
-        return personRepository.findById(id);
-    }
-
-    public void removeById(Long id) {
-        personRepository.deleteById(id);
-    }
-
-    public void update(Person newPerson, Long id) {
-        newPerson.setId(id);
-        personRepository.save(newPerson);
-    }
-
-    public void insert(Person person) {
-        personRepository.save(person);
+    PersonService(CrudRepository<Person, Long> repository) {
+        super(repository);
     }
 }
