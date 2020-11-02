48e37daba47eb2dfda8f23aaf0d459c186062eb1
sforaaleksander <sforaaleksander@gmail.com>
2020-11-02 13:59:53 +0100

    remove unused imports

--- src/main/java/com/codecool/moviesapi/controller/MovieController.java
+++ src/main/java/com/codecool/moviesapi/controller/MovieController.java
@@ -5,7 +5,6 @@ import com.codecool.moviesapi.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
 
 @RestController
 @RequestMapping("/movies")
