22d9245873e94b0230b0de30bb7cf67e00a7f9a1
MateuszGolda <mateusz.golda1993@gmail.com>
2020-11-02 17:10:38 +0100

    generify controller

---
+++ src/main/java/com/codecool/moviesapi/controller/GenericController.java
@@ -0,0 +1,39 @@
+package com.codecool.moviesapi.controller;
+
+import com.codecool.moviesapi.service.GenericService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+public abstract class GenericController<T> {
+    private GenericService service;
+
+    GenericController(GenericService service) {
+        this.service = service;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Iterable<T> getAll() {
+        return service.getAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Optional<T> getById(@PathVariable Long id) {
+        return service.getById(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteById(@PathVariable Long id) {
+        service.removeById(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public void update(@RequestBody T newObject, @PathVariable Long id) {
+        service.update(newObject, id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void insert(@RequestBody T entity) {
+        service.insert(entity);
+    }
+}
--- src/main/java/com/codecool/moviesapi/controller/MovieController.java
+++ src/main/java/com/codecool/moviesapi/controller/MovieController.java
@@ -1,43 +1,18 @@
 package com.codecool.moviesapi.controller;
 
 import com.codecool.moviesapi.entity.Movie;
-import com.codecool.moviesapi.service.MovieService;
+import com.codecool.moviesapi.service.GenericService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/movies")
-public class MovieController {
-    private MovieService movieService;
-
-    @RequestMapping(method = RequestMethod.GET)
-    public Iterable<Movie> getAllMovies() {
-        return movieService.getAllMovies();
-    }
+public class MovieController extends GenericController<Movie> {
 
     @Autowired
-    public void setMovieService(MovieService movieService) {
-        this.movieService = movieService;
-    }
-
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public Movie getMovieById(@PathVariable Long id) {
-        return movieService.getMovieById(id);
-    }
-
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public void deleteMovieById(@PathVariable Long id) {
-        movieService.removeMovieById(id);
-    }
-
-    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public void updateMovie(@RequestBody Movie newMovie, @PathVariable Long id) {
-        movieService.updateMovie(newMovie, id);
-    }
-
-    @RequestMapping(method = RequestMethod.POST)
-    public void insertMovie(@RequestBody Movie movie) {
-        movieService.insertMovie(movie);
+    MovieController(@Qualifier("movieService") GenericService<Movie> service) {
+        super(service);
     }
 }
