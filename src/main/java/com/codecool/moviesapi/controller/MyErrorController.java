093564a14316ff89d97c126c689fbc57caac91ff
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-11-03 21:31:32 +0100

    add sendMailIfServerError method

--- src/main/java/com/codecool/moviesapi/controller/MyErrorController.java
+++ src/main/java/com/codecool/moviesapi/controller/MyErrorController.java
@@ -1,7 +1,10 @@
 package com.codecool.moviesapi.controller;
 
 import com.codecool.moviesapi.service.GenericService;
+import com.codecool.moviesapi.service.LoggerService;
+import com.codecool.moviesapi.service.MailingService;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -13,6 +16,8 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MyErrorController implements ErrorController {
+    MailingService mailingService;
+    LoggerService loggerService;
     static Logger log = Logger.getLogger(GenericService.class.getName());
 
     @RequestMapping("/error")
@@ -23,17 +28,34 @@ public class MyErrorController implements ErrorController {
 
         if (status != null) {
             String error = status.toString();
-
             message = error + " " + HttpStatus.valueOf(Integer.parseInt(error)).getReasonPhrase();
             log.info(message);
+            sendMailIfServerError(status);
             return message;
         }
         message = "unknown error";
         return message;
     }
 
+    private void sendMailIfServerError(Object status) {
+        int statusCode = Integer.parseInt(status.toString());
+        if (statusCode == HttpStatus.NOT_FOUND.value()) {
+            mailingService.sendDefaultSeverErrorMessage(loggerService.getTenLastRowsFromLogFile());
+        }
+    }
+
     @Override
     public String getErrorPath() {
         return null;
+    }
+
+    @Autowired
+    public void setMailingService(MailingService mailingService) {
+        this.mailingService = mailingService;
+    }
+
+    @Autowired
+    public void setLoggerService(LoggerService loggerService) {
+        this.loggerService = loggerService;
     }
 }
