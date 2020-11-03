2a3078f5411184a16435b923b102b0f0b00a440a
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-11-03 20:01:55 +0100

    implement mailing service

---
+++ src/main/java/com/codecool/moviesapi/service/MailingService.java
@@ -0,0 +1,32 @@
+package com.codecool.moviesapi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailingService extends SimpleMailMessage {
+    private JavaMailSender emailSender;
+    private static final String BOSS_EMAIL = "noreplymoviesapi@gmail.com";
+    private static final String MACHINE_EMAIL = "noreplymoviesapi@gmail.com";
+    private static final String ERROR_500 = "500 INTERNAL SERVER ERROR";
+
+    public void sendSimpleMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(MACHINE_EMAIL);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+
+    public void sendDefaultSeverErrorMessage(String text){
+        sendSimpleMessage(BOSS_EMAIL, ERROR_500, text);
+    }
+
+    @Autowired
+    public void setEmailSender(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+}
