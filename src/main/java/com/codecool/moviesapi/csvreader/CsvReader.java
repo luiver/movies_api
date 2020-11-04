7901ad127d874124faed09cd167a229819b0fa63
sforaaleksander <sforaaleksander@gmail.com>
2020-11-04 13:08:52 +0100

    extract csvreader functionality

--- src/main/java/com/codecool/moviesapi/csvreader/CsvReader.java
+++ src/main/java/com/codecool/moviesapi/csvreader/CsvReader.java
@@ -2,7 +2,6 @@ package com.codecool.moviesapi.csvreader;
 
 import com.codecool.moviesapi.entity.Country;
 import com.codecool.moviesapi.entity.Person;
-import com.codecool.moviesapi.service.CountryService;
 import com.codecool.moviesapi.service.GenericService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,20 +10,10 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Date;
 
 @Component
-public class CsvReader {
+public abstract class CsvReader {
     BufferedReader reader;
-    GenericService<Person> personService;
-    GenericService<Country> countryService;
-
-
-    @Autowired
-    public CsvReader(GenericService<Person> personService, GenericService<Country> countryService) {
-        this.personService = personService;
-        this.countryService = countryService;
-    }
 
     public void setUp(String fileName) {
         try {
@@ -34,24 +23,12 @@ public class CsvReader {
         }
         try {
             makeImports();
+            System.out.println("Import successful");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void makeImports() throws IOException {
-        String line;
-        String[] elements;
-        Person person;
-        while ((line = reader.readLine()) != null) {
-            elements = line.split(",");
-            System.out.println(elements[3]);
-            Date date = Date.valueOf(elements[3]);
-            Country country = ((CountryService) countryService).getByName(elements[4]);
-            person = new Person(elements[0], elements[1], elements[2], date, country);
-            personService.insert(person);
-        }
-        System.out.println("Import successful");
-    }
+    protected abstract void makeImports() throws IOException ;
 }
 
