b05129ba337f51bfd2aa6a85c919b20bb3661bdf
MateuszGolda <mateusz.golda1993@gmail.com>
2020-11-04 14:58:46 +0100

    use specialized RequestMappings

--- src/main/java/com/codecool/moviesapi/controller/GenericController.java
+++ src/main/java/com/codecool/moviesapi/controller/GenericController.java
@@ -2,10 +2,7 @@ package com.codecool.moviesapi.controller;
 
 import com.codecool.moviesapi.entity.Indexable;
 import com.codecool.moviesapi.service.GenericService;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 public abstract class GenericController<T extends Indexable> {
     private final GenericService<T> service;
@@ -14,27 +11,27 @@ public abstract class GenericController<T extends Indexable> {
         this.service = service;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public Iterable<T> getAll() {
         return service.getAll();
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "/{id}")
     public T getById(@PathVariable Long id) {
         return service.getById(id);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping(value = "/{id}")
     public void deleteById(@PathVariable Long id) {
         service.removeById(id);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    @PutMapping(value = "/{id}")
     public void update(@RequestBody T newObject, @PathVariable Long id) {
         service.update(newObject, id);
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public void insert(@RequestBody T entity) {
         service.insert(entity);
     }
