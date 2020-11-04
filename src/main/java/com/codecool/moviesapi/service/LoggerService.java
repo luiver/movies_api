62caed882e0bc6a2ea3632b84a05350dfe2ae20d
MateuszGolda <mateusz.golda1993@gmail.com>
2020-11-04 10:06:11 +0100

    change getTenLastRowsFromLogFile implementation

--- src/main/java/com/codecool/moviesapi/service/LoggerService.java
+++ src/main/java/com/codecool/moviesapi/service/LoggerService.java
@@ -2,67 +2,36 @@ package com.codecool.moviesapi.service;
 
 import org.springframework.stereotype.Service;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.file.Paths;
 
 @Service
 public class LoggerService {
-    private final String DEFAULT_LOG_FILE_NAME = "logs.log";
-
+    private static final String DEFAULT_LOG_FILE_NAME = "src/main/resources/logs.log";
 
     public String getTenLastRowsFromLogFile() {
-        File logFile = new File(getLogFilePath());
-        return readLastNLinesFromFile(logFile,10);
-    }
+        StringBuilder lastTenLines = new StringBuilder();
 
-    private String getLogFilePath() {
-        ClassLoader classLoader = LoggerService.class.getClassLoader();
-        return Paths.get(classLoader.getResource(DEFAULT_LOG_FILE_NAME).getFile()).toString();
-    }
-
-    private String readLastNLinesFromFile(File file, int lines) {
-        RandomAccessFile fileHandler = null;
-        try {
-            fileHandler =
-                    new RandomAccessFile(file, "r");
-            long fileLength = fileHandler.length() - 1;
-            StringBuilder sb = new StringBuilder();
-            int line = 0;
-
-            for (long filePointer = fileLength; filePointer != -1; filePointer--) {
-                fileHandler.seek(filePointer);
-                int readByte = fileHandler.readByte();
-
-                if (readByte == 0xA) {
-                    if (filePointer < fileLength) {
-                        line = line + 1;
-                    }
-                } else if (readByte == 0xD) {
-                    if (filePointer < fileLength - 1) {
-                        line = line + 1;
-                    }
-                }
-                if (line >= lines) {
-                    break;
-                }
-                sb.append((char) readByte);
+        int linesCount = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(DEFAULT_LOG_FILE_NAME))) {
+            while (br.readLine() != null) {
+                ++linesCount;
             }
-
-            String lastLine = sb.reverse().toString();
-            return lastLine;
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
-        } finally {
-            if (fileHandler != null)
-                try {
-                    fileHandler.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
         }
+        int i = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(DEFAULT_LOG_FILE_NAME))) {
+            for (String line; (line = br.readLine()) != null; ) {
+                ++i;
+                if (linesCount - i < 10) {
+                    lastTenLines.append(line).append("\n");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lastTenLines.toString();
     }
-
 }
