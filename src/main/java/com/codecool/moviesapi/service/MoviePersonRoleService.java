29e13338f35fb107e16c48981a17608f555ab685
MateuszGolda <mateusz.golda1993@gmail.com>
2020-11-05 15:02:19 +0100

    return status 400 when inserting movie with person birth year smaller than movie year

--- src/main/java/com/codecool/moviesapi/service/MoviePersonRoleService.java
+++ src/main/java/com/codecool/moviesapi/service/MoviePersonRoleService.java
@@ -1,16 +1,42 @@
 package com.codecool.moviesapi.service;
 
 import com.codecool.moviesapi.entity.MoviePersonRole;
+import com.codecool.moviesapi.exception.BadRequestException;
+import com.codecool.moviesapi.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
+import java.util.Calendar;
+
 @Service
 public class MoviePersonRoleService extends GenericService<MoviePersonRole> {
+    private PersonService personService;
 
     @Autowired
     MoviePersonRoleService(CrudRepository<MoviePersonRole, Long> repository) {
         super(repository);
+    }
+
+    @Autowired
+    public void setMovieService(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @Override
+    public void insert(MoviePersonRole moviePersonRole) {
+        int movieYear = moviePersonRole.getMovieYear();
+        Date personBirthDate = personService.getById(moviePersonRole.getPerson().getId()).getDateOfBirth();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(personBirthDate);
+        int personBirthYear = cal.get(Calendar.YEAR);
+
+        if (movieYear < personBirthYear) {
+            throw new BadRequestException("movie year is before person birth year");
+        }
+
+        super.insert(moviePersonRole);
     }
 
     @Override
