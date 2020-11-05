d3fda50bf19c3d8102d905c64293031bc4e20643
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-11-05 23:43:14 +0100

    add new exception

---
+++ src/main/java/com/codecool/moviesapi/exception/UnprocessableEntityException.java
@@ -0,0 +1,17 @@
+package com.codecool.moviesapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ResponseStatus(code = HttpStatus.UNPROCESSABLE_ENTITY, reason = "Unprocessable Entity")
+public class UnprocessableEntityException extends RuntimeException {
+    public UnprocessableEntityException(String message) {
+        System.out.println(message);
+    }
+
+    public UnprocessableEntityException() {
+
+    }
+}
+
